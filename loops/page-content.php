c65516b92ef402e219a49c24a80c587baa7848c1
Philipp <philippkanter@gmail.com>
2018-04-11 20:51:13 +0200

    featured image fix

--- loops/page-content.php
+++ loops/page-content.php
@@ -6,11 +6,17 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-    <header class="mb-4 border-bottom">
-      <h1>
-        <?php the_title()?>
-      </h1>
-    </header>
+    <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+    <div class="container full-width text-white">
+    <div class="featured-background sub-page" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
+      <div class="container h-100 my-auto p-5">
+        <div class="row h-100">
+          <header class="col-sm my-auto border-bottom">
+            <h1 class="mt-1"><?php the_title()?></h1>
+          </header>
+        </div>
+      </div>
+    </div>
     <main>
       <?php the_content()?>
       <?php wp_link_pages(); ?>
--- page.php
+++ page.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 <?php if ( !is_front_page() ) : ?> <!-- Standardseite -->
-  <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-  <div class="featured-background" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
-  <div class="container my-5">
+  <div class="container mb-5">
     <div class="row">
       <div class="col-sm mt-5">
         <div id="content" role="main">
@@ -15,9 +13,9 @@
 <?php if ( is_front_page() ) : ?><!-- Startseite -->
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-      <header class="masthead text-center text-white d-flex">
+      <header class="text-center text-white d-flex">
         <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-        <div class="featured-background" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
+        <div class="featured-background front-page" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
         <div class="container my-auto">
           <div class="row">
             <div class="col-lg-10 mx-auto">
