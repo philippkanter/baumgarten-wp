5f8364c870daf5a1fd94bb527f4c7905a21e40cf
Philipp Kanter <philippkanter@gmail.com>
2018-04-11 16:39:02 +0200

    teaserbild für pages

--- page.php
+++ page.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
-<?php if ( !is_front_page() ) : ?> 
+<?php if ( !is_front_page() ) : ?> <!-- Standardseite -->
+  <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+  <div class="featured-background" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
   <div class="container my-5">
     <div class="row">
       <div class="col-sm mt-5">
@@ -7,11 +9,10 @@
           <?php get_template_part('loops/page-content'); ?>
         </div><!-- /#content -->
       </div>
-      <!-- <?php // get_sidebar(); ?> -->
     </div><!-- /.row -->
   </div><!-- /.container -->
 <?php endif; ?>
-<?php if ( is_front_page() ) : ?>
+<?php if ( is_front_page() ) : ?><!-- Startseite -->
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
       <header class="masthead text-center text-white d-flex">
