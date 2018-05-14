fafbb8610d0deb65bfeb0cae161747ea1e58e7bf
Philipp <philippkanter@gmail.com>
2018-05-15 00:43:37 +0200

    Header ausblenden

--- loops/page-content.php
+++ loops/page-content.php
@@ -6,17 +6,17 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-    <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-    <div class="container full-width text-white">
-    <div class="featured-background sub-page" style="background-image: url('<?php echo $thumb['0'];?>');"></div>
+    <!-- <div class="container full-width text-white">
+    <?php // $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+    <div class="featured-background sub-page" style="background-image: url('<?php // echo $thumb['0'];?>');"></div>
       <div class="container h-100 my-auto py-5">
         <div class="row h-100">
-          <header class="col my-auto border-bottom border-3">
-            <h1><?php the_title()?></h1>
-          </header>
         </div>
       </div>
-    </div>
+    </div> -->
+    <header class="pb-5">
+      <h1><?php the_title()?></h1>
+    </header>
     <main class="my-5">
       <?php the_content()?>
       <?php wp_link_pages(); ?>
