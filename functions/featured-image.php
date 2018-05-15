17d7d2d0ee3e50b59b8aec02fedf7df91683ea4d
Philipp <philippkanter@gmail.com>
2018-05-16 00:25:26 +0200

    featured image shortcode

--- functions/featured-image.php
+++ functions/featured-image.php
@@ -1,2 +1,10 @@
 <?php
-// add_image_size( 'custom-size', 220, 180 );
+add_image_size( 'custom-size', 1920, 570, array( 'center', 'center') );
+
+add_shortcode('featured-image', 'thumbnail_in_content');
+
+function thumbnail_in_content($atts) {
+    global $post;
+
+    return get_the_post_thumbnail($post->ID, $size = 'custom-size', $attr = 'featured-image');
+}
