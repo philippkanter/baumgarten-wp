661e0057fd9bd79f905d1fb1dac636c18e5b5822
Philipp Kanter <philippkanter@gmail.com>
2018-06-06 18:54:59 +0200

    cleanup + add local repos

--- functions/cleanup.php
+++ functions/cleanup.php
@@ -14,12 +14,28 @@ if ( ! function_exists('b4st_cleanup_head') ) {
     remove_action('wp_head', 'feed_links', 2);
     remove_action('wp_head', 'feed_links_extra', 3);
     remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);+
+    remove_action('wp_head', 'wp_shortlink_header', 10, 0);
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+    remove_action('wp_head', 'start_post_rel_link', 10, 0);
     remove_action('wp_head', 'print_emoji_detection_script', 7);
-	  remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('tiny_mce_plugins', 'remove_tinymce_emoji');
   }
 }
 add_action('init', 'b4st_cleanup_head');
+
+function remove_tinymce_emoji($plugins){
+  if (!is_array($plugins)){
+    return array();
+  }
+	return array_diff($plugins, array('wpemoji'));
+}
 
 // Show less info to users on failed login for security.
 // (Will not let a valid username be known.)
