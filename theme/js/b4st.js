573093f0c3418ba845d869c97426434adf6aadc0
Philipp <philippkanter@gmail.com>
2020-02-19 20:45:20 +0100

    smoth scrolling added + small css fixes

--- theme/js/b4st.js
+++ theme/js/b4st.js
@@ -8,8 +8,22 @@
 
 	$(document).ready(function() {
 
+		// Remove margin from hidden anchor
+
 		$( ".card-text" ).has( ".anchor" ).addClass( "mb-0" );
 
+		// Smooth scrolling
+
+		$('a[href^="#"]').on('click',function(e) {
+			e.preventDefault();
+			var target = this.hash;
+			var $target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 900, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
 
 		// Comments
 
